d503cb32034c96d544f34c5a16525f5c5e69810f
VioletViolaVi <vivian.v18723@gmail.com>
2020-05-10 23:04:42 +0000

    practiced JavaScript

---
+++ practice.js
@@ -0,0 +1,76 @@
+console.log('hello world');
+console.log(3-4);
+// alert("what's up y'all!");
+console.error("oh no it's an error");
+console.warn("so this is how a warning works?");
+
+let a = 11;
+console.log(a);
+
+a = 10;
+/* First a was 10... */
+console.log("A is: " + a);
+
+/* then it was 15 */
+a = 15;
+console.log("A is: " + a);
+
+a = 10; b = 15; c = 20;
+
+console.log(a,b,c)
+
+let banana = 10; apple = 20; grapes = 30; pear = 40; orange = 50;
+console.log (banana,apple,grapes,pear,orange);
+
+banana = "yellow"; apple = "red"; grapes = "purple"; pear = "green"; orange = "orange";
+console.log (banana,apple,grapes,pear,orange);
+
+let theExes = "3 people";
+console.log(theExes);
+
+let theNanny = "the lady named Fran!";
+console.log (theNanny);
+
+x = 42;
+console.log(x); // however, this is bad practice 'cause u didn't use a keyword for a brand new variable!!!!
+
+let myAge;
+console.log(myAge);
+
+let myDateOfBirth = "18th May 1994";
+console.log(myDateOfBirth);
+
+var Happy = "capital letter for the word Happy";
+var happy = "smallcase letter for the word happy";
+var weAreAllHappy = "this used camelCase to say everyone is happy!";
+console.log("this one has the capital H:" + Happy);
+console.log("this one has the smallcase h:" + happy);
+console.log(weAreAllHappy);
+
+var e = 1; f = 2; g = 3; h = 4;
+console.log("e means: " + e);
+console.log("f means: " + f);
+console.log("g means: " + g);
+console.log("h means: " + h);
+
+let zay = 60;
+zay ++ ;
+console.log(zay);
+
+let pie = 50;
+let iceCream = -pie;
+console.log(iceCream);
+
+var now  = 2;
+var later = +2;
+console.log(later);
+
+console.log(1/+0);
+console.log(+1/0);
+console.log(1/-0);
+console.log(-1/0);
+
+console.log(+0/1);
+console.log(0/+1);
+console.log(-0/1);
+console.log(0/-1);
