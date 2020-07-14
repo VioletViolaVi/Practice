1190ee8eafc07184bfa421435b52312ee901e81f
VioletViolaVi <vivian.v18723@gmail.com>
2020-07-14 19:54:37 +0000

    Practicing using python.

---
+++ try.py
@@ -0,0 +1,398 @@
+# import math
+
+# print("hello you")
+# print("0----")
+# print(" |||| ")
+# print("*" * 10)
+
+# print("*"*70)
+
+# price = 10
+# price = 20
+# rating = 40.9
+# name = "viv"
+# is_published = True
+# is_not_published = False
+# print(price)
+# print(rating)
+# print(name)
+# print(is_published)
+# print(is_not_published)
+
+# print("*"*70)
+
+# name = "John Smith"
+# age = 20
+# is_new_patient = True
+# print(name)
+# print(age)
+# print(is_new_patient)
+
+# print("*"*70)
+
+# name = input("Hello! Tell me your name please! :) ")
+# print(f"Hey {name}. Nice to meet you! :D")
+# print("Hey {}. Nice to meet you! :D ".format(name))
+
+# print("*"*70)
+
+# fav_colour = input("What's your favourite colour in the WHOLE WIDE world! :) ")
+# home = input("Where do you live on this massive planet? ")
+# print(f"Ah! That's sooo COOL! But, I don't really like {fav_colour} so much.")
+# print(f"I don't know where {home} is located but it sounds nice lol.")
+
+# print("*"*70)
+
+# birth_year = input("What year were you born?: ")
+# print(type(birth_year))
+# age = 2020 - int(birth_year)
+# print(type(age))
+# print(f"You are {age} years old or will be {age} years old this year.")
+
+
+# print("*"*70)
+
+# weight = input("What is your weight in pounds? ")
+# kilo_weight = float(weight)/2.2046
+# print(f"Your weight is {kilo_weight}kg in kilograms.")
+
+# print("*"*70)
+
+# string = "This is my string. It's mine!"
+# print(string)
+# string2 = "We are \"fighting\" dreamers!"
+# print(string2)
+# text = """
+# Hi.
+# This is a letter/text.
+# Did you know you could do this?
+# signed, Vi.
+# """
+# print(text)
+# practice = "This is a sentence. What could it mean?"
+# print(practice[0:19])
+# print(len(practice))
+
+# print("*"*70)
+
+# first_name = "Dil"
+# last_name = "Pickle"
+# message = first_name + " [" + last_name + "] " + "is a rugrat!"
+# print(message)
+# msg = f"{first_name} [{last_name}] is a rugrat!"
+# print(msg)
+
+# print("*"*70)
+
+# lyric = "The night's are lonely, the days are so sad and I just keep thinking about the days that we had and I'm missing you and nobody knows it but me!"
+# print(len(lyric))
+# print(lyric.upper())
+# print(lyric.lower())
+# print(lyric)
+# print(lyric.find("days"))
+# print(lyric.replace("days", "WHAT IS THIS!!!"))
+# print(lyric.replace("t", "Q"))
+# print("Sad" in lyric)
+# print("sad" in lyric)
+# print(lyric.title())
+
+# print("*"*70)
+
+# print(10**3)
+# print(10//3)
+# increment = 10
+# increment *= 3
+# print(increment)
+# bodmas = 10+3*2
+# print(bodmas)
+# bodmas2 = 10+3*2**2
+# print(bodmas2)
+# bodmas3 = (10+3)*2**2
+# print(bodmas3)
+
+# print("*"*70)
+
+# x = 2.5
+# print(abs(-6652.99))
+# print(math.comb(2, 4))
+
+# print("*"*70)
+
+# is_hot = False
+# is_warm = True
+
+# if is_hot:
+#     print("It's hot today!")
+#     print("drink plenty of water")
+# elif is_warm:
+#     print("it's warm")
+# else:
+#     print("It's a cold day.")
+#     print("wear more clothes")
+
+# print("enjoy your day")
+
+# print("*"*70)
+
+# buyer_has_good_credit = False
+
+# if buyer_has_good_credit:
+#     ten_percent = (10/100)*1000000
+#     print(ten_percent)
+#     print(f"£{ten_percent} will be the down payment.")
+# else:
+#     twenty_percent = (20/100)*1000000
+#     print(twenty_percent)
+#     print(f"£{twenty_percent} will be the down payment.")
+
+# print("*"*70)
+
+# pizza = False
+# cabbage = True
+
+# if pizza and not cabbage:
+#     print("I like eating pizza! Cabbage is nasty.")
+# elif cabbage and not pizza:
+#     print("I like to eat cabbage. No pizza.")
+# elif pizza and cabbage:
+#     print("I like them both.")
+# else:
+#     print("I don't like either. I like cake.")
+
+# temp = 50
+
+# if temp >= 50:
+#     print("it's boiling")
+# else:
+#     print("it's not boiling")
+
+# print("*"*70)
+
+# name = "Vivvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvviiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn"
+# name = "Vivian"
+# name = "Vi"
+# if len(name) < 3:
+#     print("name must be at least 3 characters")
+# elif len(name) > 50:
+#     print("name must not exceed 50 characters")
+# else:
+#     print("name looks okay!")
+# print(len(name))
+
+# print("*"*70)
+
+# weight = int(input("What's your weight? "))
+# kilos_or_pounds = input("Is this in kilos (K) or pounds(P)? ")
+
+# if kilos_or_pounds == "k" or "K":
+#     kilos = weight*2.2
+#     print(f"You are {kilos} kilos.")
+# else:
+#     print(f"You are {weight} pounds.")
+
+# print("*"*70)
+
+# i = 1
+# while i <= 5:
+#     print(f"{i}, then the next number is...")
+#     i += 1
+
+# i = 1
+# while i <= 5:
+#     print(("**"*i) + ("*"*i))
+#     i += 1
+# print("done")
+
+# print("Three tries! Guess the number!")
+
+# number_to_guess = 90
+# guess_maxed_out = 3
+# guess_tries = 0
+
+# while guess_tries < guess_maxed_out:
+#     guessing = int(input("Enter your guess "))
+#     guess_tries += 1
+#     if guessing == number_to_guess:
+#         print("you win!!! :D")
+#         break
+# else:
+#     print("sorry but you failed :()")
+
+# print("*"*70)
+
+# print("Let's play the car game! :) Shout for help if needed!")
+# already_started = False
+# while True:
+#     car = input("").lower()
+#     if car == "start":
+#         if already_started:
+#             print("Hey! What are you doing, it's already started!")
+#         else:
+#             started = True
+#             print("Starting the car")
+#     elif car == "stop":
+#         if not already_started:
+#             print("Stoping the car")
+#         else:
+#             already_started = False
+#             print("The already stopped though...")
+#     elif car == "help":
+#         print("""
+# start - start car
+# stop - stop car
+# quit - leave game
+# help - ask for help
+#         """)
+#     elif car == "quit":
+#         print("Exiting the car")
+#         break
+#     else:
+#         message = "Eh! I don't understand, what did you just type? :|"
+#         print(message)
+
+# print("*"*70)
+
+# for item in "Vivian":
+#     print(item)
+
+# for item in ["Vivian", "Kath", "Woo"]:
+#     print(item)
+
+# for item in [1, 2, 3, 4, 5]:
+#     print(item)
+
+# for item in range(0, 11, 2):
+#     print(item)
+
+# prices = [10, 20, 30]
+# total = 0
+
+# for price in prices:
+#     total = total + price
+# print(f"Total:{total}")
+
+# print("*"*70)
+
+# for x in range(4):
+#     for y in range(3):
+#         print(f"({x}, {y})")
+
+# numbers = [5, 2, 5, 2, 2]
+# for number in numbers:
+#     outcome = ""
+#     for number_of_xs in range(number):
+#         outcome += "x"
+#     print(outcome)
+
+# numbers = [2, 2, 2, 2, 5]
+# for number in numbers:
+#     outcome = ""
+#     for number_of_xs in range(number):
+#         outcome += "x"
+#     print(outcome)
+
+# numbers = [5, 2, 5, 2, 5]
+# for number in numbers:
+#     outcome = ""
+#     for number_of_xs in range(number):
+#         outcome += "x"
+#     print(outcome)
+
+# print("*"*70)
+
+# names = ["Woo", "Soo", "Somi", "Yugi", "Gohan"]
+# names[3] = "Yuri"
+# print(names[:4])
+
+# numbers = [1, 2, 3, 4, 70, 5, 6, 7, 8, 9, 10]
+# max_number = max(numbers)
+# print(max_number)
+
+# numbers_second_go = [40, 3535, 4, 8457464, 234, 0, 4  ]
+# highest_number = numbers_second_go[0]
+
+# for number_from_the_list in numbers_second_go:
+#     if highest_number < number_from_the_list:
+#         highest_number = number_from_the_list
+# print(highest_number)
+
+# print("*"*70)
+
+# lists_in_a_list = [
+#     [2, 4, 6],
+#     [8, 10, 12],
+#     [14, 16, 18]
+# ]
+
+# # print(lists_in_a_list[2][1])
+# # print(lists_in_a_list[1][2])
+# # print(lists_in_a_list[1][0])
+# # print(lists_in_a_list[0][2])
+
+# for row in lists_in_a_list:
+#     for number in row:
+#         print(number)
+
+
+print("*"*70)
+
+
+
+print("*"*70)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def check_engine(oil, coolant, gasoline):
+#     if oil == 1:
+#         if gasoline == 1:
+#             if coolant == 1:
+#                 return "engine started!"
+#         else:
+#             return "gas or coolant issue"
+#     else:
+#         return "oil issue"
+
+# def check_engine(oil, coolant, gasoline):
+#     if oil == 1:
+#         if gasoline and coolant == 1:
+#             return "engine started!"
+#         else:
+#             return "gas or coolant issue"
+#     else:
+#         return "oil issue"
