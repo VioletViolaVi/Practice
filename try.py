0a9fa29913cfe6607285aa79da4adf23bf07218a
VioletViolaVi <vivian.v18723@gmail.com>
2020-07-21 22:57:29 +0000

    More practice with python.

--- try.py
+++ try.py
@@ -478,6 +478,19 @@
 # from flask import Flask
 
 
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+    if __name__ == "__main__":
+        app.run(host=os.environ.get("IP"), port=int(
+            os.environ.get("PORT")), debug=True)
 
 
 
